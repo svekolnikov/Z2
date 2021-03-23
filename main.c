fa1e4e6f47432394a7b5a5f0788e6232beb3aac0
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-03-23 13:17:11 +0300

    +

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    seed = 11;
+    n = 50;
+    m1 = 15;
+    m2 = 5;
+
+
+    // scanf();
+
+    //validation
+    if( seed < 0 || n<10 || n>100)
+        return -1;
+
+
+
+
+    while(1){}
     return 0;
 }
