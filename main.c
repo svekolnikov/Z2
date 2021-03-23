b013fccaa71d671d2b794f145feb44aeb96fce4c
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-03-23 13:21:14 +0300

    +

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int GetRandom(int from, int to)
+{
+    SEED = SEED * 16807 % R_MAX;
+    return from + (int) SEED % (to - from + 1);
+}
+
 int main()
 {
     seed = 11;
@@ -8,11 +14,10 @@ int main()
     m1 = 15;
     m2 = 5;
 
-
-    // scanf();
+    //scanf("%d %d %d %d", seed,n, m1, m2);
 
     //validation
-    if( seed < 0 || n<10 || n>100)
+    if( seed < 0 || n < 10 || n > 100 || (m1+m2)/2 <= n/2)
         return -1;
 
 
